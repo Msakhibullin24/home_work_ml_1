e3a5241980fb4322d6234c2bbb6271b458a720fe
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-11-02 13:54:28 +0300

    zadanie2

--- tests.py
+++ tests.py
@@ -2,8 +2,4 @@ biletik = list(input("введите номер своего билета  "))
 if int(biletik[0]) +int(biletik[1]) + int(biletik[2]) == int(biletik[3]) +int(biletik[4]) + int(biletik[5]):
     print('Счастливый')
 else:
-    print('Обычный')
-
-
-
-
+    print('Несчастливый')
