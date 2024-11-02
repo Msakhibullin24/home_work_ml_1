7a914cf94819f9cda0107463d2c4a349b5ec3212
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-11-02 10:49:07 +0300

    ff

---
+++ lesson1.py
@@ -0,0 +1,35 @@
+#a = int(input("Введите первое число "))
+#b= int(input("Введите второе число "))
+#c= int(input("Введите третье число "))
+
+#cat = [a, b, c ]
+
+
+#print(max(cat))
+
+#print(min(cat))
+
+
+#last = (cat)
+
+#last.remove(max(last))
+#last.remove(min(last))
+
+#print(cat)
+#print(x, y, sep="\n")
+
+#rint(last)
+
+
+a = int(input("Введите первое число "))
+b = int(input("Введите второе число "))
+c = int(input("Введите третье число "))
+
+maximum = max(a, b, c)
+minimum = min(a, b, c)
+seredina = a + b + c - maximum - minimum
+
+
+print(maximum)
+print(minimum)
+print(seredina)
