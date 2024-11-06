a7da465de115c59ac019433f70526fde3a2d65b2
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-11-06 07:56:34 +0300

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,26 +1,3 @@
-#a = int(input("Введите первое число "))
-#b= int(input("Введите второе число "))
-#c= int(input("Введите третье число "))
-
-#cat = [a, b, c ]
-
-
-#print(max(cat))
-
-#print(min(cat))
-
-
-#last = (cat)
-
-#last.remove(max(last))
-#last.remove(min(last))
-
-#print(cat)
-#print(x, y, sep="\n") так то по идеи не пригодилось
-
-#rint(last)
-#это варианты ранее которое пытался примениит выше этой строки....
-
 a = int(input("Введите первое число "))
 b = int(input("Введите второе число "))
 c = int(input("Введите третье число "))
@@ -28,7 +5,6 @@ c = int(input("Введите третье число "))
 maximalka = max(a, b, c)#максимальное значение тут считаем в общем
 minimalka = min(a, b, c)#минимальное значение тут ищем из всех
 seredina = a + b + c - maximalka - minimalka #тут уже ищем оставшееся число чтобы его напечатать
-
 
 print(maximalka)
 print(minimalka)
